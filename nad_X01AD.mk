f7d7bc0d05e240b31a79345efc3e10c3ab9aecd2
Brothereye <99174654+BrotherI01@users.noreply.github.com>
2022-02-13 22:35:59 +0530

    Update nad_X01AD.mk

--- nad_X01AD.mk
+++ nad_X01AD.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/asus/X01AD/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/nad/config/common.mk)
+$(call inherit-product, vendor/nusantara/config/common.mk)
 
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
