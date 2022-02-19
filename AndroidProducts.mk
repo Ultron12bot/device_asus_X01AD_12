26ef09d953fb09df64b12c4dd6968cb84542a509
Brothereye <99174654+BrotherI01@users.noreply.github.com>
2022-02-20 03:00:02 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/octavi_X01AD.mk
+    $(LOCAL_DIR)/cherish_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    octavi_X01AD-user \
-    octavi_X01AD-userdebug \
-    octavi_X01AD-eng
+    cherish_X01AD-user \
+    cherish_X01AD-userdebug \
+    cherish_X01AD-eng
