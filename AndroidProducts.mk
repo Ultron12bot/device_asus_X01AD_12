a4829336ab4916a5af50a0b510b5e2818a51d5ad
Brothereye <99174654+BrotherI01@users.noreply.github.com>
2022-02-23 17:40:54 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X01AD.mk
+    $(LOCAL_DIR)/corvus_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X01AD-user \
-    aosp_X01AD-userdebug \
-    aosp_X01AD-eng
+    corvus_X01AD-user \
+    corvus_X01AD-userdebug \
+    corvus_X01AD-eng
