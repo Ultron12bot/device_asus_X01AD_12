5e1255b74715e9c824c1a425b05b3614886222fe
Brothereye <99174654+BrotherI01@users.noreply.github.com>
2022-02-23 20:44:45 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_X01AD.mk
+    $(LOCAL_DIR)/nad_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_X01AD-user \
-    bliss_X01AD-userdebug \
-    bliss_X01AD-eng
+    nad_X01AD-user \
+    nad_X01AD-userdebug \
+    nad_X01AD-eng
